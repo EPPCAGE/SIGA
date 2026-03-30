0ec03d7566e2e866bd59630fb7c44852d18be052
EPPCAGE <Felipe_tourinho@yahoo.com.br>
2026-03-30 17:02:37 -0300

    Prepara branch com seguranca para PRD

--- public-config.js
+++ public-config.js
@@ -2,18 +2,13 @@ globalThis.__SIGA_RUNTIME__ = {
   apiUrl: '/api',
   auth: {
     // local | entra
-    mode: 'entra',
+    mode: 'local',
     entra: {
-      clientId: '07efc1ef-b923-404e-b056-0be8dbfea66d',
-      tenantId: '83bd090b-756e-4a02-a512-e5ea02c03041',
+      clientId: '',
+      tenantId: '',
       redirectUri: globalThis.location.origin,
-      adminEmails: ['felipet@sefaz.rs.gov.br'],
-      editorEmails: [
-        'edisonw@sefaz.rs.gov.br',
-        'marcelomc@sefaz.rs.gov.br',
-        'andreabs@sefaz.rs.gov.br',
-        'amandasp@sefaz.rs.gov.br',
-      ],
+      adminEmails: ['local@admin'],
+      editorEmails: [],
     },
   },
 };
